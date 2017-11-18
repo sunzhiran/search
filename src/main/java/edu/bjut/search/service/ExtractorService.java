95694104df8c082cad4c6b311bf44f3c66756235
sunzhiran <982473454@qq.com>
2017-11-18 19:23:46 +0800

    add source checking

--- src/main/java/edu/bjut/search/service/ExtractorService.java
+++ src/main/java/edu/bjut/search/service/ExtractorService.java
@@ -7,17 +7,30 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Service
 public class ExtractorService {
     private static Logger logger = LoggerFactory.getLogger(ExtractorService.class);
 
     public DocText getTextFromOnline(String source) {
-        //TODO source合法性检测
+        if ( ! checkSource(source)) {
+            logger.warn("数据源地址不合法。");
+            return null;
+        }
         return TextExtractor.extractText(source, SourceTypeEnum.OTHERS_ONLINE);
     }
 
     public DocText getTextFromLocal(String source) {
         return TextExtractor.extractText(source, SourceTypeEnum.OTHERS_LOCAL);
+    }
+
+
+    private boolean checkSource(String source) {
+        Pattern pattern = Pattern.compile("(http|https|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]");
+        Matcher matcher = pattern.matcher(source);
+        return matcher.matches();
     }
 
 }
